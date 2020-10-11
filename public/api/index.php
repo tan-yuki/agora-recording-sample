ff82a1c6aff592c13a465ce25364ebe10a3ff2bc
tan-yuki <a0673040@gmail.com>
2020-10-11 16:15:51 +0900

    Bugfix

--- public/api/index.php
+++ public/api/index.php
@@ -8,7 +8,7 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 // Load env file
 // if production, set environment variables directly by Heroku console.
-if ($_ENV['ENV'] !== 'PRODUCTION') {
+if ($_ENV['AGORA_APP_ENV'] !== 'PRODUCTION') {
     $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
     $dotenv->load();
 }

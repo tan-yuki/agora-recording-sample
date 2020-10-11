e3b3c17a2de38bf3914793513fc09c11839424ce
tan-yuki <a0673040@gmail.com>
2020-10-11 12:56:43 +0900

    Resolved warning in feature test

--- tests/FeatureTest/Api/StartRecordingApiTest.php
+++ tests/FeatureTest/Api/StartRecordingApiTest.php
@@ -6,22 +6,23 @@ namespace FeatureTest\Api;
 
 use AgoraServer\Application\Controller\Recording\StartRecording\StartRecordingRequest;
 use AgoraServer\Application\Controller\Recording\StartRecording\StartRecordingUseCase;
+use AgoraServer\Application\Controller\Recording\StartRecording\StartResponseDto;
 use AgoraServer\Domain\Agora\Entity\Recording\RecordingId;
 use AgoraServer\Domain\Agora\Entity\Recording\ResourceId;
 use FeatureTest\FeatureBaseTestCase;
 
 class StartRecordingApiTest extends FeatureBaseTestCase
 {
-    private static array $returnUseCaseValue;
+    private static StartResponseDto $returnUseCaseValue;
 
     public static function setUpBeforeClass(): void
     {
         parent::setUpBeforeClass();
 
-        self::$returnUseCaseValue= [
+        self::$returnUseCaseValue= new StartResponseDto(
             new ResourceId('this_is_resource_id'),
-            new RecordingId('this_is_recording_id'),
-        ];
+            new RecordingId('this_is_recording_id')
+        );
     }
 
     protected function setUp(): void
@@ -42,7 +43,7 @@ class StartRecordingApiTest extends FeatureBaseTestCase
     /**
      * @test
      */
-    public function return_secure_token()
+    public function return_200_and_response()
     {
         $response = $this->runApp('POST', '/recording/start', json_encode([
             StartRecordingRequest::PARAM_USER_ID => '1234',
@@ -54,9 +55,9 @@ class StartRecordingApiTest extends FeatureBaseTestCase
         $this->assertArrayHasKey('sid', $responseArray);
         $this->assertArrayHasKey('resourceId', $responseArray);
 
-        $expectsResponse = self::$returnUseCaseValue;
-        $this->assertSame($expectsResponse[0]->value(), $responseArray['sid']);
-        $this->assertSame($expectsResponse[1]->value(), $responseArray['res']);
+        $expectsResponse = self::$returnUseCaseValue->toArray();
+        $this->assertSame($expectsResponse['sid'], $responseArray['sid']);
+        $this->assertSame($expectsResponse['resourceId']->value(), $responseArray['res']);
     }
 
 
